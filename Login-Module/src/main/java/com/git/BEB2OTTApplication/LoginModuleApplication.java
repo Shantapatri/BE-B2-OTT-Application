d021796b6eb39d1e97c04fc1c0916c777174aa05
Muthuraj647 <muthurajmuthuraj32@gmail.com>
2022-09-07 18:12:54 +0530

    Login Module Setup

---
+++ Login-Module/src/main/java/com/git/BEB2OTTApplication/LoginModuleApplication.java
@@ -0,0 +1,13 @@
+package com.git.BEB2OTTApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginModuleApplication.class, args);
+	}
+
+}
